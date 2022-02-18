1db0a937c4d54798589088ebe4a225ba4f5031f8
WhiteFan <admin@baifan97.cn>
2022-02-18 12:24:49 +0800

    Initial commit

---
+++ app/src/main/java/com/whitefan/jdlite/bean/UserBean.kt
@@ -0,0 +1,36 @@
+package com.whitefan.jdlite.bean
+
+import java.math.BigDecimal
+
+class UserBean {
+    var nickName = ""
+    var headImageUrl = ""
+    var userLevel = ""
+    var levelName = ""
+    var beanNum = ""
+    var todayBean = 0
+    var ago1Bean = 0
+    var ago2Bean = 0
+    var ago3Bean = 0
+    var ago4Bean = 0
+
+    var farmName = ""
+    var treeEnergy = ""
+    var treeTotalEnergy = ""
+    var treeImageUrl = ""
+
+    var hb = "--"
+    var jxiang = "--"
+    var gqhb = "--"
+
+
+    var page = 1
+
+    var jxRed = BigDecimal(0)
+    var jdRed = BigDecimal(0)
+    var jsRed = BigDecimal(0)
+
+    var jxRedGQ = BigDecimal(0)
+    var jdRedGQ = BigDecimal(0)
+    var jsRedGQ = BigDecimal(0)
+}
---
+++ app/src/main/java/com/whitefan/jdlite/bean/VersionBean.kt
@@ -0,0 +1,9 @@
+package com.whitefan.jdlite.bean
+
+
+class VersionBean {
+    var content_url: String? = null
+    var release: String? = null
+    var content: String? = null
+    var widgetTip = ""
+}
